427c4ce9c716bced01143a0cbd667542a5660882
mentab <25363363+mentab@users.noreply.github.com>
2018-11-10 15:07:37 +0100

    add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
-let path = require('path');
+const path = require('path');
+const moduleObj = {
+    loaders: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loaders: ["babel-loader"],
+        }
+    ],
+};
 const client = {
     entry: {
-        'client': './src/client/index.js'
+        'client': './src/client/index.js',
     },
     target: 'web',
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist/public')
-    }
+    },
+    module: moduleObj
 };
 const server = {
     entry: {
@@ -17,6 +27,7 @@ const server = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
-    }
-};
+    },
+    module: moduleObj
+}
 module.exports = [client, server];
